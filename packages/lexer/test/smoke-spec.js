f24d9c392cba203b288b48b74038f1b3a53df1c6
JounQin <admin@1stg.me>
2023-08-02 03:45:48 +0000

    refactor: multiline quote start-end should match

--- packages/lexer/test/smoke-spec.js
+++ packages/lexer/test/smoke-spec.js
@@ -5,7 +5,12 @@ describe("The Toml Tools Lexer", () => {
   it("can tokenize a sample Toml", () => {
     const input = `# This is a TOML document.
 title = "TOML Example"
-description = """
+description1 = '''
+Some
+multiline
+description
+'''
+description2 = """
 Some
 multiline
 description
@@ -13,7 +18,7 @@ description
 `;
     const lexingResult = tokenize(input);
     expect(lexingResult.errors).to.be.empty;
-    expect(lexingResult.tokens).to.have.lengthOf(10);
+    expect(lexingResult.tokens).to.have.lengthOf(14);
     const tokenImages = lexingResult.tokens.map(tok => tok.image);
     expect(tokenImages).to.deep.equal([
       "# This is a TOML document.",
@@ -22,7 +27,11 @@ description
       "=",
       '"TOML Example"',
       "\n",
-      "description",
+      "description1",
+      "=",
+      "'''\nSome\nmultiline\ndescription\n'''",
+      "\n",
+      "description2",
       "=",
       '"""\nSome\nmultiline\ndescription\n"""',
       "\n"
